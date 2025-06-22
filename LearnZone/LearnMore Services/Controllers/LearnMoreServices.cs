fa3213ef25afb9a0bca4e5df174f6d47fcc1c796
2KE1E <2KE1E@LAPTOP-DQK3MQGI>
2025-06-22 12:36:57 +0530

    service added

---
+++ LearnZone/LearnMore Services/Controllers/LearnMoreServices.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LearnZoneDAL;
+using System.Runtime.CompilerServices;
+
+namespace LearnMore_Services.Controllers
+{
+
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class LearnMoreServices:ControllerBase
+
+    {
+        LearnMoreRepositary repositary;
+        public LearnMoreServices(LearnMoreRepositary repositary)
+        {
+            this.repositary = repositary;
+
+        }
+        [HttpGet]
+        public int Login(String username , string password)
+        {
+            int res = 0;
+            try
+            {
+                res = repositary.Login(username, password);
+            }
+            catch(Exception e)
+            {
+                res = -1;
+            }
+            return (res);
+        }
+
+
+    }
+}
