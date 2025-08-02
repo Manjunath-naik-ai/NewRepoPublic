0e40e71f8443fe5afd15cc44b4944b28fe9a9cf6
2KE1E <raddymanju317@gmail.com>
2025-08-02 14:49:08 +0530

    modification on enroll methods

--- LearnZone/LearnMoreApp/src/app/user-dash-board/user-dash-board.ts
+++ LearnZone/LearnMoreApp/src/app/user-dash-board/user-dash-board.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+    import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Lzservice } from '../services/lzservice';
@@ -36,4 +36,25 @@ export class UserDashBoard implements OnInit {
     });
 
   }
+
+  Enroll(courseId: number) {
+    console.log("Raw course.id:", courseId, "Type:", typeof courseId);
+
+    const user: Iuser = JSON.parse(sessionStorage.getItem('user') || '{}');
+        if (!user ) {
+      alert("Please log in to enroll in a course.");
+        return;
+        }
+    this.lzservice.enrollCourse(user.id, courseId).subscribe({
+      next: (response) => {
+        console.log('Enrollment successful:', response);
+        alert("Successfully enrolled in the course.");
+      },
+      error: (error) => {
+        console.error('Error enrolling in course:', error);
+        alert("Failed to enroll in the course. Please try again later.");
+      }
+    });
+  }
+
 }
