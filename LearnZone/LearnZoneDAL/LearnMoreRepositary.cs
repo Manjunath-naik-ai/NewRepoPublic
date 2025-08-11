de00539807d32b19d2a2c3f1123f8717bbcae634
2KE1E <raddymanju317@gmail.com>
2025-08-11 19:11:45 +0530

    dal changes

--- LearnZone/LearnZoneDAL/LearnMoreRepositary.cs
+++ LearnZone/LearnZoneDAL/LearnMoreRepositary.cs
@@ -434,6 +434,39 @@ namespace LearnZoneDAL
             return chapters;
         }
         #endregion
+
+
+        #region feedbackpost
+        public int insertfeedback(String mesg)
+        {
+
+            try
+            {
+                using (var context = new ElearningDbContext())
+                {
+                    var feedback = new Feedback
+                    {
+                        UserId = userId,
+                        CourseId = courseId,
+                        Rating = rating,
+                        Comments = comments,
+                        SubmittedAt = DateTime.UtcNow
+                    };
+
+                    context.Feedbacks.Add(feedback);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error (you can replace with your logger)
+                Console.WriteLine($"Error adding feedback: {ex.Message}");
+                return false;
+            }
+
+        }
+        #endregion
     }
 }
 
